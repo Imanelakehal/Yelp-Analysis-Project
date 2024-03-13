be1bb5e12ae9859b2921c7e80dbd2ea756c55183
NesleVincent <elsenvincentius@gmail.com>
2024-03-13 16:51:27 +0800

    connected to review

--- app.py
+++ app.py
@@ -2,23 +2,26 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-# Update the SQLAlchemy configuration with the correct MySQL URI
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:imane@192.168.56.106:3306/users'
-# Assuming MySQL server is running locally
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:admin@192.168.26.129:3306/metastore'
 db = SQLAlchemy(app)
 
-# Define your User model
-class User(db.Model):
-    __tablename__ = 'users'
+# Define your model for the reviews table
+class Review(db.Model):
+    __tablename__ = 'review_counts'
+    rev_year = db.Column(db.Integer, primary_key=True)
+    count = db.Column(db.Integer)
 
-    user_id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(255))
-
+# Route for the home page
 @app.route('/')
-def index():
-    # Fetch all users from the database
-    total_users = User.query.all()
-    return render_template('index.html', total_users=total_users)
+def render_website():
+    return render_template('index.html')
+
+# Route for the reviews page
+@app.route('/templatesReviews.html')
+def reviews():
+    # Query the database to retrieve data from the reviews table
+    data = Review.query.order_by(Review.rev_year.asc()).all()
+    return render_template('Reviews.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
