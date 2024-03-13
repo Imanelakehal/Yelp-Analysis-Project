465a3a06780f064bbf403b0c507047fa5dc0eb18
Imanelakehal <manimaimane@gmail.com>
2024-03-13 16:14:30 +0800

    connected to MySQL database.

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
-from flask import Flask, render_template, send_from_directory, redirect, url_for
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.static_folder = 'static'  # Set static folder path
+# Update the SQLAlchemy configuration with the correct MySQL URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:imane@192.168.56.106:3306/users'
+# Assuming MySQL server is running locally
+db = SQLAlchemy(app)
 
-# Route for the home page
+# Define your User model
+class User(db.Model):
+    __tablename__ = 'users'
+
+    user_id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(255))
+
 @app.route('/')
-def render_website():
-    return render_template('index.html')
-
-@app.route('/businesses')
-def page1():
-    return render_template('businesses.html')
-
-@app.route('/Users')
-def page2():
-    return render_template('Users.html')
-
+def index():
+    # Fetch all users from the database
+    total_users = User.query.all()
+    return render_template('index.html', total_users=total_users)
 
 if __name__ == '__main__':
     app.run(debug=True)
