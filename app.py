b6a71e3b3811d9f17797ef0ab5c21b9c5e79b097
NesleVincent <elsenvincentius@gmail.com>
2024-03-13 16:59:12 +0800

    adding imane's database into the code

--- app.py
+++ app.py
@@ -2,14 +2,28 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:admin@192.168.26.129:3306/metastore'
-db = SQLAlchemy(app)
 
-# Define your model for the reviews table
-class Review(db.Model):
+# Connect to the first MySQL database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:admin@192.168.26.129:3306/metastore'
+db1 = SQLAlchemy(app)
+
+# Connect to the second MySQL database
+app.config['SQLALCHEMY_BINDS'] = {
+    'users': 'mysql://root:imane@192.168.56.106:3306/users'
+}
+db2 = SQLAlchemy(app)
+
+# Define your models for the first database
+class Review(db1.Model):
     __tablename__ = 'review_counts'
-    rev_year = db.Column(db.Integer, primary_key=True)
-    count = db.Column(db.Integer)
+    rev_year = db1.Column(db1.Integer, primary_key=True)
+    count = db1.Column(db1.Integer)
+
+# Define your models for the second database
+class User(db2.Model):
+    __tablename__ = 'users'
+    id = db2.Column(db2.Integer, primary_key=True)
+    username = db2.Column(db2.String(50), unique=True)
 
 # Route for the home page
 @app.route('/')
@@ -17,7 +31,7 @@ def render_website():
     return render_template('index.html')
 
 # Route for the reviews page
-@app.route('/templatesReviews.html')
+@app.route('/reviews')
 def reviews():
     # Query the database to retrieve data from the reviews table
     data = Review.query.order_by(Review.rev_year.asc()).all()
